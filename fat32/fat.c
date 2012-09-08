52ca260b9301f6c07645e0f6c2e099a6b2efacca
ph147 <directx2000@gmx.de>
2012-09-08 14:36:09 +0200

    Makefile, fixed fat_strncmp, implemented mkdir, create new cluster if necessary, writes to backup fat, fixed bunch of char/uint8_t warnings, check for valid filename (make uppercase), added bunch of possible last-cluster-markers, functions didn't return success, int i --> size_t

--- fat32/fat.c
+++ fat32/fat.c
@@ -15,8 +15,12 @@
 #define FAT_ARCHIVE 0x20
 
 #define FAT_DELETED 0xe5
+#define FAT_LAST_CLUSTER 0xfffffff
+#define FAT_EMPTY 0x0
 
 #define FILENAME "/home/test/fs.fat"
+
+const char *fat_legal_chars = "!#$%&'()-@^_`{}~";
 
 typedef struct
 {
@@ -75,6 +79,7 @@ typedef struct
 typedef struct
 {
     FILE *fp;
+    uint32_t length_of_fat;
     uint32_t start_of_fat;
     uint32_t start_of_data;
     uint32_t current_directory_addr;
@@ -84,34 +89,38 @@ typedef struct
     bootsector_t boot;
 } mountpoint_t;
 
-static int isempty(char *s, size_t n);
-static void fat_put_str(char *s);
+static int isempty(uint8_t *s, size_t n);
+static void fat_put_str(uint8_t *s);
 static uint32_t next_cluster(mountpoint_t *mount, uint32_t cluster);
-static uint32_t is_last_cluster(mountpoint_t *mount, uint32_t cluster);
+static uint32_t is_last_cluster(uint32_t cluster);
 static uint32_t cluster_to_addr(mountpoint_t *mount, uint32_t cluster);
 static void print_file(mountpoint_t *mount, file_t *file);
+static int valid_file(file_t *file);
+static int valid_filename(char *s, int len, uint8_t att);
+static int fat_cmp(char c, char d);
+static int fat_strncmp(char *fs_file, char *cmp_file, int len, int max);
+static void split_filename(char *filename, int *fname, int *fext);
+static uint16_t next_free_cluster(mountpoint_t *mount, uint16_t last_cluster);
+static void fat_write_to_fat(mountpoint_t *mount, uint16_t cluster, uint32_t value);
+static int fat_create_file(mountpoint_t *mount, char *filename, uint8_t att, uint16_t cluster);
+static void next_dir_entry(mountpoint_t *mount);
+static void uppercase(char *s, int len);
+
 void parent_dir(mountpoint_t *mount);
 int fat_chdir(mountpoint_t *mount, char *dirname);
-int valid_file(mountpoint_t *mount, file_t *file);
-void fat_ls_file(mountpoint_t *mount, file_t *file);
-static int fat_cmp(char c, char d);
-static int fat_strncmp(char *fs_file, char *cmp_file, int len);
-void split_filename(char *filename, int *fname, int *fext);
+void fat_ls_file(file_t *file);
 int fat_search_file(mountpoint_t *mount, char *filename, file_t *file);
 void fat_cat(mountpoint_t *mount, char *filename);
-void next_dir_entry(mountpoint_t *mount);
 void fat_rm(mountpoint_t *mount, char *filename);
 // TODO void fat_mv(mountpoint_t *mount, char *source, char *destination);
-uint16_t next_free_cluster(mountpoint_t *mount);
-void fat_write_to_fat(mountpoint_t *mount, uint16_t cluster, uint32_t value);
-void fat_create_file(mountpoint_t *mount, char *filename);
+int fat_mkdir(mountpoint_t *mount, char *filename);
 void fat_touch(mountpoint_t *mount, char *filename);
 void fat_ls_dir(mountpoint_t *mount);
 void mount_fat32(mountpoint_t *mount, char *filename);
 void umount(mountpoint_t *mount);
 void commandline(mountpoint_t *mount);
 
-static int isempty(char *s, size_t n)
+static int isempty(uint8_t *s, size_t n)
 {
     size_t i;
     for (i = 0; i < n; ++i)
@@ -123,7 +132,7 @@ static int isempty(char *s, size_t n)
     return 1;
 }
 
-static void fat_put_str(char *s)
+static void fat_put_str(uint8_t *s)
 {
     int count = 0;
     while (*s != ' ' && count < 8)
@@ -134,10 +143,24 @@ static void fat_put_str(char *s)
     }
 }
 
-static uint32_t is_last_cluster(mountpoint_t *mount, uint32_t cluster)
+static void uppercase(char *s, int len)
 {
-    return ((cluster == 0xfff) || (cluster == 0xffff) || (cluster == 0xff8)
-            || (cluster == 0xfffffff) || (cluster ==0xfff8));
+    int count = 0;
+    while (*s && count < len)
+    {
+        if (*s >= 'a' && *s <= 'z')
+            *s += 'A'-'a';
+        ++s;
+        ++count;
+    }
+}
+
+static uint32_t is_last_cluster(uint32_t cluster)
+{
+    return ((cluster == 0xfff) || (cluster == 0xffff)
+            || (cluster == 0xff8) || (cluster == 0xfff8)
+            || ((cluster & 0xfffffff) == 0xffffff8)
+            || ((cluster & 0xfffffff) == 0xfffffff));
 }
 
 /* returns 0 on EOF */
@@ -146,7 +169,7 @@ static uint32_t next_cluster(mountpoint_t *mount, uint32_t cluster)
     uint32_t tmp;
     fseek(mount->fp, mount->start_of_fat + 4*cluster, SEEK_SET);
     fread(&tmp, sizeof(uint32_t), 1, mount->fp);
-    if (is_last_cluster(mount, tmp))
+    if (is_last_cluster(tmp))
         return 0;
     return tmp;
 }
@@ -159,7 +182,7 @@ static uint32_t cluster_to_addr(mountpoint_t *mount, uint32_t cluster)
 
 static void print_file(mountpoint_t *mount, file_t *file)
 {
-    int count = 0;
+    uint32_t count = 0;
     uint32_t cluster = file->cluster;
     uint32_t last_pos;
     char buf[mount->bytes_per_cluster+1];
@@ -198,7 +221,7 @@ int fat_chdir(mountpoint_t *mount, char *dirname)
     {
         if (!(dir.attributes & FAT_SUBDIR))
         {
-            printf("%s: Not a directory.\n", dirname);
+            printf("ch: %s: Not a directory.\n", dirname);
             return 0;
         }
         if (!dir.cluster)
@@ -222,9 +245,10 @@ int fat_chdir(mountpoint_t *mount, char *dirname)
     {
         printf("cd: %s: Directory not found.\n", dirname);
     }
+    return 1;
 }
 
-int valid_file(mountpoint_t *mount, file_t *file)
+static int valid_file(file_t *file)
 {
     if ((file->attributes & FAT_READONLY) &&
             (file->attributes & FAT_HIDDEN) &&
@@ -234,14 +258,51 @@ int valid_file(mountpoint_t *mount, file_t *file)
     return 1;
 }
 
-void fat_ls_file(mountpoint_t *mount, file_t *file)
+static int is_in(char c, const char *s)
+{
+    while (*s)
+    {
+        if (*s == c)
+            return 1;
+        ++s;
+    }
+    return 0;
+}
+
+// TODO ~1, ~2
+static int valid_filename(char *s, int len, uint8_t att)
+{
+    int count = 0;
+    char c;
+
+    if (att == FAT_SUBDIR && s[0] == '.')
+        return 1;
+    uppercase(s, len);
+    while ((c = *s) && count < len)
+    {
+        if ((c >= 'A' && c <= 'Z')
+                || (c >= '0' && c <= '9')
+                || is_in(c, fat_legal_chars)
+                //|| (c >= 128 && c <= 255)
+           )
+        {
+            ++s;
+            ++count;
+        }
+        else
+            return 0;
+    }
+    return 1;
+}
+
+void fat_ls_file(file_t *file)
 {
     int hours, minutes, seconds;
     int day, month, year;
 
     if (file->short_file_name[0] == FAT_DELETED)
         return;
-    if (!valid_file(mount, file))
+    if (!valid_file(file))
         return;
 
     seconds = (file->time & 0x1f) * 2;
@@ -275,7 +336,7 @@ static int fat_cmp(char c, char d)
     return LOWERCASE(c) == LOWERCASE(d);
 }
 
-static int fat_strncmp(char *fs_file, char *cmp_file, int len)
+static int fat_strncmp(char *fs_file, char *cmp_file, int len, int max)
 {
     int count = 0;
 
@@ -286,11 +347,21 @@ static int fat_strncmp(char *fs_file, char *cmp_file, int len)
         ++count;
     }
     if (count == len)
-        return 1;
+    {
+        if ((count < max && *fs_file == ' ') || count == max)
+            return 1;
+        else if (count > max)
+        {
+            printf("Illegal file name. This shouldn't have happened.\n");
+            return 0;
+        }
+        else
+            return 0;
+    }
     return 0;
 }
 
-void split_filename(char *filename, int *fname, int *fext)
+static void split_filename(char *filename, int *fname, int *fext)
 {
     int count = 0;
 
@@ -322,7 +393,7 @@ void split_filename(char *filename, int *fname, int *fext)
 // mount->fp at position of found file
 int fat_search_file(mountpoint_t *mount, char *filename, file_t *file)
 {
-    int i;
+    size_t i;
     uint16_t cluster = mount->current_directory_cluster;
     uint32_t addr = mount->current_directory_addr;
 
@@ -336,14 +407,14 @@ int fat_search_file(mountpoint_t *mount, char *filename, file_t *file)
         for (i = 0; i < mount->bytes_per_cluster/32; ++i)
         {
             fread(file, sizeof(file_t), 1, mount->fp);
-            if (!valid_file(mount, file))
+            if (!valid_file(file))
                 continue;
             if (file->short_file_name[0] == 0x00)
                 continue;
-            if (fat_strncmp(file->short_file_name, filename, fname))
+            if (fat_strncmp(file->short_file_name, filename, fname, 8))
             {
                 if ((!fext && file->short_file_extension[0] == ' ') ||
-                    (fat_strncmp(file->short_file_extension, filename+fname+1, fext)))
+                    (fat_strncmp(file->short_file_extension, filename+fname+1, fext, 3)))
                 {
                     fseek(mount->fp, -sizeof(file_t), SEEK_CUR);
                     return 1;
@@ -356,6 +427,7 @@ int fat_search_file(mountpoint_t *mount, char *filename, file_t *file)
             break;
         addr = cluster_to_addr(mount, cluster);
     }
+    return 0;
 }
 
 void fat_rm(mountpoint_t *mount, char *filename)
@@ -377,6 +449,48 @@ void fat_rm(mountpoint_t *mount, char *filename)
         printf("rm: %s: File not found.\n", filename);
 }
 
+static int has_dot(char *s)
+{
+    while (*s)
+    {
+        if (*s == '.')
+            return 1;
+        ++s;
+    }
+    return 0;
+}
+
+int fat_mkdir(mountpoint_t *mount, char *filename)
+{
+    uint16_t cluster, next;
+
+    cluster = mount->current_directory_cluster;
+    if (!cluster)
+        cluster = 2;
+
+    if (has_dot(filename))
+    {
+        printf("mkdir: %s: Illegal name.\n", filename);
+        return 0;
+    }
+
+    next = next_free_cluster(mount, 0);
+
+    if (!fat_create_file(mount, filename, FAT_SUBDIR, next))
+    {
+        printf("mkdir: %s: Error.\n", filename);
+        return 0;
+    }
+
+    fat_chdir(mount, filename);
+    if (!fat_create_file(mount, ".", FAT_SUBDIR, next))
+        return 0;
+    if (!fat_create_file(mount, "..", FAT_SUBDIR, cluster))
+        return 0;
+    fat_chdir(mount, "..");
+    return 1;
+}
+
 void fat_cat(mountpoint_t *mount, char *filename)
 {
     file_t file;
@@ -393,9 +507,10 @@ void fat_cat(mountpoint_t *mount, char *filename)
 }
 
 // sets file pointer to next free directory entry
-void next_dir_entry(mountpoint_t *mount)
+static void next_dir_entry(mountpoint_t *mount)
 {
-    int i;
+    size_t i;
+    uint16_t next;
     uint16_t cluster = mount->current_directory_cluster;
     uint32_t addr = mount->current_directory_addr;
     file_t file;
@@ -413,15 +528,15 @@ void next_dir_entry(mountpoint_t *mount)
                 return;
             }
         }
-        cluster = next_cluster(mount, cluster);
-        printf(">>>%d\n", cluster);
-        if (!cluster)
-            ; // TODO create new cluster
-        addr = cluster_to_addr(mount, cluster);
+        next = next_cluster(mount, cluster);
+        if (!next)
+            next = next_free_cluster(mount, cluster);
+        addr = cluster_to_addr(mount, next);
     }
 }
 
-uint16_t next_free_cluster(mountpoint_t *mount)
+// TODO drive full
+static uint16_t next_free_cluster(mountpoint_t *mount, uint16_t last_cluster)
 {
     uint32_t tmp;
     uint16_t cluster = 0;
@@ -431,20 +546,28 @@ uint16_t next_free_cluster(mountpoint_t *mount)
     {
         fread(&tmp, sizeof(uint32_t), 1, mount->fp);
         if (tmp == 0x00)
+        {
+            if (last_cluster)
+                fat_write_to_fat(mount, last_cluster, (uint32_t) cluster);
+            fat_write_to_fat(mount, cluster, (uint32_t) FAT_LAST_CLUSTER);
             return cluster;
+        }
         ++cluster;
     }
 }
 
-void fat_write_to_fat(mountpoint_t *mount, uint16_t cluster, uint32_t value)
+static void fat_write_to_fat(mountpoint_t *mount, uint16_t cluster, uint32_t value)
 {
     fseek(mount->fp, mount->start_of_fat + 4*cluster, SEEK_SET);
     if (!fwrite(&value, sizeof(uint32_t), 1, mount->fp))
         printf("Write error: Could not write to FAT.\n");
+    // write to backup fat
+    fseek(mount->fp, mount->length_of_fat - 4, SEEK_CUR);
+    if (!fwrite(&value, sizeof(uint32_t), 1, mount->fp))
+        printf("Write error: Could not write to FAT.\n");
 }
 
-// TODO create new cluster if full
-void fat_create_file(mountpoint_t *mount, char *filename)
+static int fat_create_file(mountpoint_t *mount, char *filename, uint8_t att, uint16_t cluster)
 {
     file_t file;
     int fname;
@@ -454,6 +577,14 @@ void fat_create_file(mountpoint_t *mount, char *filename)
     // TODO valid filename, ~1, ~2, ...
 
     split_filename(filename, &fname, &fext);
+    
+    if (fname > 8 || fext > 3
+            || !valid_filename(filename, fname, att)
+            || !valid_filename(filename+fname+1, fext, att))
+    {
+        printf("Error: %s: Illegal file name.\n", filename);
+        return 0;
+    }
 
     memset(file.short_file_name, ' ', 8);
     memset(file.short_file_extension, ' ', 3);
@@ -469,7 +600,7 @@ void fat_create_file(mountpoint_t *mount, char *filename)
     }
     else
         sprintf(file.short_file_extension, "   ");
-    file.attributes = FAT_ARCHIVE;
+    file.attributes = att;
     file.created_time = 0x0; //00:00:00
     file.created_date = 0xd72; //1986-11-18
     file.time = 0x0; //00:00:00
@@ -478,15 +609,15 @@ void fat_create_file(mountpoint_t *mount, char *filename)
     file.nt = 0;
     file.extended_attr = 0;
     file.file_size = 0;
-    file.cluster = 0;
-    /*
-    file.cluster = next_free_cluster(mount)-1;
-    fat_write_to_fat(mount, file.cluster, 0);
-    */
+    file.cluster = cluster;
 
     next_dir_entry(mount);
     if (!fwrite(&file, sizeof(file_t), 1, mount->fp))
+    {
         printf("touch: %s: Write error.\n", filename);
+        return 0;
+    }
+    return 1;
 }
 
 void fat_touch(mountpoint_t *mount, char *filename)
@@ -507,13 +638,13 @@ void fat_touch(mountpoint_t *mount, char *filename)
     }
     else
     {
-        fat_create_file(mount, filename);
+        fat_create_file(mount, filename, FAT_ARCHIVE, FAT_EMPTY);
     }
 }
 
 void fat_ls_dir(mountpoint_t *mount)
 {
-    int i;
+    size_t i;
     uint16_t cluster = mount->current_directory_cluster;
     uint32_t addr = mount->current_directory_addr;
     file_t file;
@@ -527,7 +658,7 @@ void fat_ls_dir(mountpoint_t *mount)
             fread(&file, sizeof(file_t), 1, mount->fp);
             if (file.short_file_name[0] == 0x00)
                 continue;
-            fat_ls_file(mount, &file);
+            fat_ls_file(&file);
         }
         cluster = next_cluster(mount, cluster);
         if (!cluster)
@@ -555,8 +686,8 @@ void mount_fat32(mountpoint_t *mount, char *filename)
 
     mount->bytes_per_cluster = bytes_per_sector * sectors_per_cluster;
     mount->start_of_fat = bytes_per_sector * reserved_sectors;
-    mount->start_of_data = mount->start_of_fat +
-        bytes_per_sector * sectors_per_fat * 2;
+    mount->length_of_fat = bytes_per_sector * sectors_per_fat;
+    mount->start_of_data = mount->start_of_fat + mount->length_of_fat * 2;
     mount->current_directory_addr = mount->start_of_data;
     mount->current_directory[0] = '/';
     mount->current_directory[1] = '\0';
@@ -574,7 +705,7 @@ void commandline(mountpoint_t *mount)
     char *args[2];
     for (;;)
     {
-        printf("sh:%s$ ", mount->current_directory);
+        printf("FAT32-TEST-sh:%s$ ", mount->current_directory);
         fgets(buf, 127, stdin);
         args[0] = strtok(buf, " \n");
         args[1] = strtok(NULL, " \n");
@@ -587,7 +718,7 @@ void commandline(mountpoint_t *mount)
         {
             if (!args[1])
             {
-                printf("%s: Command not found.\n", args[0]);
+                printf("sh: %s: Command not found.\n", args[0]);
                 continue;
             }
             if (!strcmp(args[0], "cd"))
@@ -598,9 +729,11 @@ void commandline(mountpoint_t *mount)
                 fat_touch(mount, args[1]);
             else if (!strcmp(args[0], "rm"))
                 fat_rm(mount, args[1]);
+            else if (!strcmp(args[0], "mkdir"))
+                fat_mkdir(mount, args[1]);
             else
             {
-                printf("%s: Command not found.\n", args[0]);
+                printf("sh: %s: Command not found.\n", args[0]);
                 continue;
             }
         }
